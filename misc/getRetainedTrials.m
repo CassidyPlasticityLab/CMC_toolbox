f9442e8a594fb41b128d5bc59ec675b119910c29
jimark13 <72454619+jimark13@users.noreply.github.com>
2023-07-15 18:26:03 -0400

    Update getRetainedTrials.m
    Resolved "Matrix index is out of range for deletion" error when this code is performed in parallel.
    
    Added a callout for when single epochs are removed with no bracket call (i.e. "pop_select( EEG, 'notarial', NUMBER)) rather than i.e. "pop_select( EEG, 'notarial', [NUMBERS]))

--- misc/getRetainedTrials.m
+++ misc/getRetainedTrials.m
@@ -1,29 +1,43 @@
 function [retainedTrials, totalTrials] = getRetainedTrials(EEG)
-
-if contains(EEG.history, "pop_select( EEG, 'notrial',[")
-    %Specific trial number saved
-    popSelectStr = "EEG = pop_select( EEG, 'notrial',[";
-    k = strfind(EEG.history, popSelectStr);
-    col = size(k, 2);
-    for i =1:col
-        l{i} = extractAfter(EEG.history, k(i)+strlength(popSelectStr)-1);
-        m{i} = extractBefore(l{i}, ']');
-        rejectedEpochs{i} = str2num(m{1, i});
-    end
     
-    for i = 1:length(rejectedEpochs)
-        totalTrials(i) = length(rejectedEpochs{1, i});
+    if contains(EEG.history, "pop_select( EEG, 'notrial',[")
+        popSelectStr = "EEG = pop_select( EEG, 'notrial',[";
+        k = strfind(EEG.history, popSelectStr);
+        col = numel(k);
+        rejectedEpochs = cell(1, col);
+        
+        for i = 1:col
+            l = extractAfter(EEG.history, k(i) + strlength(popSelectStr) - 1);
+            m = extractBefore(l, ']');
+            rejectedEpochs{i} = str2num(m);
+        end
+        
+        totalTrials = sum(cellfun(@numel, rejectedEpochs)) + numel(EEG.epoch);
+        retainedTrials = 1:totalTrials;
+        
+        for i = 1:col
+            retainedTrials = setdiff(retainedTrials, rejectedEpochs{i});
+        end
+    elseif contains(EEG.history, "pop_select( EEG, 'notrial',")
+        popSelectStr = "EEG = pop_select( EEG, 'notrial',";
+        k = strfind(EEG.history, popSelectStr);
+        col = numel(k);
+        rejectedEpochs = cell(1, col);
+        
+        for i = 1:col
+            l = extractAfter(EEG.history, k(i) + strlength(popSelectStr) - 1);
+            m = extractBefore(l, ']');
+            rejectedEpochs{i} = str2num(m);
+        end
+        
+        totalTrials = sum(cellfun(@numel, rejectedEpochs)) + numel(EEG.epoch);
+        retainedTrials = 1:totalTrials;
+        
+        for i = 1:col
+            retainedTrials = setdiff(retainedTrials, rejectedEpochs{i});
+        end
+    else
+        retainedTrials = 1:size(EEG.data, 3);
+        totalTrials = numel(retainedTrials);
     end
-    
-    totalTrials = sum(totalTrials)+length(EEG.epoch);
-    
-    retainedTrials = [1:totalTrials];
-    col = size(rejectedEpochs, 2);
-    for i = 1:col
-        retainedTrials(rejectedEpochs{1, i}(1, :)) = [];
-    end
-else 
-    retainedTrials = 1:size(EEG.data, 3);
-    totalTrials = size(EEG.data, 3)
-end
 end
