e590925571eef4f3f9ab1a760a45623f53632220
jimark13 <72454619+jimark13@users.noreply.github.com>
2024-12-19 15:30:26 -0500

    Add files via upload

--- getExtract/getExtractCohValues.m
+++ getExtract/getExtractCohValues.m
@@ -1,69 +1,62 @@
-function [cohSpctrm, coh, cohValsOneLine, cohLabelOneLine] = getExtractCohValues(fdFourierCoh, chanClass, freqBands)
-load ROI
-cohSpctrm = fdFourierCoh.cohspctrm;
+function [cohSpctrm, coh, cohValsOneLine, cohLabelOneLine] = getExtractCohValues(fdFourierCoh, freqBands)
+    load ROI;
 
-cohSpctrmDelta = squeeze(mean(cohSpctrm(:, :, (round(fdFourierCoh.freq) >=freqBands.delta(1) & round(fdFourierCoh.freq) <=freqBands.delta(end))), 3));
-cohSpctrmTheta = squeeze(mean(cohSpctrm(:, :, (round(fdFourierCoh.freq) >=freqBands.theta(1) & round(fdFourierCoh.freq) <=freqBands.theta(end))), 3));
-cohSpctrmAlphaMu = squeeze(mean(cohSpctrm(:, :, (round(fdFourierCoh.freq) >=freqBands.alphaMu(1) & round(fdFourierCoh.freq) <=freqBands.alphaMu(end))), 3));
-cohSpctrmBeta1 = squeeze(mean(cohSpctrm(:, :, (round(fdFourierCoh.freq) >=freqBands.beta1(1) & round(fdFourierCoh.freq) <=freqBands.beta1(end))), 3));
-cohSpctrmBeta2 = squeeze(mean(cohSpctrm(:, :, (round(fdFourierCoh.freq) >=freqBands.beta2(1) & round(fdFourierCoh.freq) <=freqBands.beta2(end))), 3));
-cohSpctrmGamma1 = squeeze(mean(cohSpctrm(:, :, (round(fdFourierCoh.freq) >=freqBands.gamma1(1) & round(fdFourierCoh.freq) <=freqBands.gamma1(end))), 3));
-cohSpctrmGamma2 = squeeze(mean(cohSpctrm(:, :, (round(fdFourierCoh.freq) >=freqBands.gamma2(1) & round(fdFourierCoh.freq) <=freqBands.gamma2(end))), 3));
+    % Initialize structures for coherence spectra
+    cohSpctrm = fdFourierCoh.cohspctrm;
 
-[cohSpctrmDelta] = add_deleted_chans_square_matrix(cohSpctrmDelta);
-[cohSpctrmTheta] = add_deleted_chans_square_matrix(cohSpctrmTheta);
-[cohSpctrmAlphaMu] = add_deleted_chans_square_matrix(cohSpctrmAlphaMu);
-[cohSpctrmBeta1] = add_deleted_chans_square_matrix(cohSpctrmBeta1);
-[cohSpctrmBeta2] = add_deleted_chans_square_matrix(cohSpctrmBeta2);
-[cohSpctrmGamma1] = add_deleted_chans_square_matrix(cohSpctrmGamma1);
-[cohSpctrmGamma2] = add_deleted_chans_square_matrix(cohSpctrmGamma2);
+    % Frequency bands
+    bandNames = fieldnames(freqBands);
+    bandData = struct();
 
-for roiIndex = 1:length(ROI)-8
-    region.delta.(ROI{1, roiIndex}) = cohSpctrmDelta(ROI{2, roiIndex}, :);
-    region.theta.(ROI{1, roiIndex}) = cohSpctrmTheta(ROI{2, roiIndex}, :);
-    region.alphaMu.(ROI{1, roiIndex}) = cohSpctrmAlphaMu(ROI{2, roiIndex}, :);
-    region.beta1.(ROI{1, roiIndex}) = cohSpctrmBeta1(ROI{2, roiIndex}, :);
-    region.beta2.(ROI{1, roiIndex}) = cohSpctrmBeta2(ROI{2, roiIndex}, :);
-    region.gamma1.(ROI{1, roiIndex}) = cohSpctrmGamma1(ROI{2, roiIndex}, :);
-    region.gamma2.(ROI{1, roiIndex}) = cohSpctrmGamma2(ROI{2, roiIndex}, :);
-end
-
-for  regionIndex = 1:length(ROI)-8
-    for roiIndex = 1:length(ROI)-8
-        coh.delta.(ROI{1, regionIndex}).(ROI{1, roiIndex}) = mean(mean(region.delta.(ROI{1, regionIndex})(:, ROI{2, roiIndex})));
-        coh.theta.(ROI{1, regionIndex}).(ROI{1, roiIndex}) = mean(mean(region.theta.(ROI{1, regionIndex})(:, ROI{2, roiIndex})));
-        coh.alphaMu.(ROI{1, regionIndex}).(ROI{1, roiIndex}) = mean(mean(region.alphaMu.(ROI{1, regionIndex})(:, ROI{2, roiIndex})));
-        coh.beta1.(ROI{1, regionIndex}).(ROI{1, roiIndex}) = mean(mean(region.beta1.(ROI{1, regionIndex})(:, ROI{2, roiIndex})));
-        coh.beta2.(ROI{1, regionIndex}).(ROI{1, roiIndex}) = mean(mean(region.beta2.(ROI{1, regionIndex})(:, ROI{2, roiIndex})));
-        coh.gamma1.(ROI{1, regionIndex}).(ROI{1, roiIndex}) = mean(mean(region.gamma1.(ROI{1, regionIndex})(:, ROI{2, roiIndex})));
-        coh.gamma2.(ROI{1, regionIndex}).(ROI{1, roiIndex}) = mean(mean(region.gamma2.(ROI{1, regionIndex})(:, ROI{2, roiIndex})));
-
+    % Extract frequency bands using loops
+    for b = 1:length(bandNames)
+        band = bandNames{b};
+        freqIdx = round(fdFourierCoh.freq) >= freqBands.(band)(1) & round(fdFourierCoh.freq) <= freqBands.(band)(2);
+        bandData.(band) = squeeze(mean(cohSpctrm(:, :, freqIdx), 3));
+        % Add deleted channels for square matrices
+        bandData.(band) = add_deleted_chans_square_matrix(bandData.(band));
     end
-end
 
-fields_A = fieldnames(coh);
-for i = 1:length(fields_A)
-    fields_B = fieldnames(coh.(fields_A{i}));
-    for j = 1:length(fields_B)
-        fields_C = fieldnames(coh.(fields_A{i}).(fields_B{j}));
+    % Create regional coherence matrices
+    region = struct();
+    for roiIndex = 1:length(ROI) - 8
+        regionName = ROI{1, roiIndex};
+        for b = 1:length(bandNames)
+            band = bandNames{b};
+            region.(band).(regionName) = bandData.(band)(ROI{2, roiIndex}, :);
+        end
     end
-end
 
-
-monsterArrayLength = length(fields_A)*length(fields_B)*length(fields_C);
-cohValsOneLine = zeros(1, monsterArrayLength);
-for i = 1:monsterArrayLength
-    for j = 1:length(fields_A)
-        for k = 1:length(fields_B)
-            for l = 1:length(fields_C)
-                cohValsOneLine(i) = coh.(fields_A{j}).(fields_B{k}).(fields_C{l});
-                cohLabelOneLine{i} = strcat(fields_A{j}, '_', fields_B{k}, '_', fields_C{l});
-                i = i+1;
+    % Calculate mean coherence between ROIs
+    coh = struct();
+    for regionIndex = 1:length(ROI) - 8
+        regionName = ROI{1, regionIndex};
+        for roiIndex = 1:length(ROI) - 8
+            roiName = ROI{1, roiIndex};
+            for b = 1:length(bandNames)
+                band = bandNames{b};
+                coh.(band).(regionName).(roiName) = ...
+                    mean(mean(region.(band).(regionName)(:, ROI{2, roiIndex})));
             end
         end
     end
+
+    % Linearize coherence values and labels
+    fields_A = fieldnames(coh);
+    cohValsOneLine = [];
+    cohLabelOneLine = {};
+    for i = 1:length(fields_A)
+        band = fields_A{i};
+        fields_B = fieldnames(coh.(band));
+        for j = 1:length(fields_B)
+            regionName = fields_B{j};
+            fields_C = fieldnames(coh.(band).(regionName));
+            for k = 1:length(fields_C)
+                roiName = fields_C{k};
+                cohValsOneLine(end+1) = coh.(band).(regionName).(roiName); % Append coherence value
+                cohLabelOneLine{end+1} = strcat(band, '_', regionName, '_', roiName); % Append label
+            end
+        end
+    end
+
 end
-
-
-cohValsOneLine = cohValsOneLine(4032:end);
-cohLabelOneLine = cohLabelOneLine(4032:end);
--- getExtract/getExtractPowerValues.m
+++ getExtract/getExtractPowerValues.m
@@ -1,95 +1,55 @@
-function [relPow, relPowValsOneLine, relPowLabelOneLine] = getExtractPowerValues(freqRelPow, freqBands, EEG, chanClass)
+function [relPow, relPowValsOneLine, relPowLabelOneLine] = getExtractPowerValues(freqRelPow, freqBands, EEG)
 
-load ROI
-% averagedTrialPow = squeeze(mean(freqRelPow.powspctrm, 1));
-% trialPowChansAdded = add_deleted_chans(averagedTrialPow, EEG.chanlocs);
-% trialPow = vertcat(trialPowChansAdded, averagedTrialPow(chanClass.EMGChans, :));
-% 
-% powDelta = trialPow(:, (round(freqRelPow.freq) >=freqBands.delta(1) & round(freqRelPow.freq) <=freqBands.delta(end)));
-% powTheta = trialPow(:, (round(freqRelPow.freq) >=freqBands.theta(1) & round(freqRelPow.freq) <=freqBands.theta(end)));
-% powAlphaMu = trialPow(:, (round(freqRelPow.freq) >=freqBands.alphaMu(1) & round(freqRelPow.freq) <=freqBands.alphaMu(end)));
-% powBeta1 = trialPow(:, (round(freqRelPow.freq) >=freqBands.beta1(1) & round(freqRelPow.freq) <=freqBands.beta1(end)));
-% powBeta2 = trialPow(:,(round(freqRelPow.freq) >=freqBands.beta2(1) & round(freqRelPow.freq) <=freqBands.beta2(end)));
-% powGamma1 = trialPow(:, (round(freqRelPow.freq) >=freqBands.gamma1(1) & round(freqRelPow.freq) <=freqBands.gamma1(end)));
-% powGamma2 = trialPow(:, (round(freqRelPow.freq) >=freqBands.gamma2(1) & round(freqRelPow.freq) <=freqBands.gamma2(end)));
-% powAllFreq = trialPow(:, :);
-% 
-% for roiIndex = 1:length(ROI)
-%     Pow.delta.(ROI{1, roiIndex}) = mean(mean(powDelta(ROI{2, roiIndex})));
-%     Pow.theta.(ROI{1, roiIndex}) = mean(mean(powTheta(ROI{2, roiIndex})));
-%     Pow.alphaMu.(ROI{1, roiIndex}) = mean(mean(powAlphaMu(ROI{2, roiIndex})));
-%     Pow.beta1.(ROI{1, roiIndex}) = mean(mean(powBeta1(ROI{2, roiIndex})));
-%     Pow.beta2.(ROI{1, roiIndex}) = mean(mean(powBeta2(ROI{2, roiIndex})));
-%     Pow.gamma1.(ROI{1, roiIndex}) = mean(mean(powGamma1(ROI{2, roiIndex})));
-%     Pow.gamma2.(ROI{1, roiIndex}) = mean(mean(powGamma2(ROI{2, roiIndex})));
-%     Pow.allFreq.(ROI{1, roiIndex}) = mean(mean(powAllFreq(ROI{2, roiIndex})));
-% end
-% fields_A = fieldnames(Pow);
-% for i = 1:length(fields_A)
-%     fields_B = fieldnames(Pow.(fields_A{i}));
-% end
-% 
-% 
-% monsterArrayLength = length(fields_A)*length(fields_B);
-% powValsOneLine = zeros(1, monsterArrayLength);
-% for i = 1:monsterArrayLength
-%     for j = 1:length(fields_A)
-%         for k = 1:length(fields_B)
-%             powValsOneLine(i) = Pow.(fields_A{j}).(fields_B{k});
-%             powLabelOneLine{i} = strcat(fields_A{j}, '_', fields_B{k});
-%             i = i+1;
-%         end
-%     end
-% end
-% 
-% 
-% 
-% powValsOneLine = powValsOneLine(256:end);
-% powLabelOneLine = powLabelOneLine(256:end);
+    % Load ROI information
+    load ROI;
 
-%Repeat the above for relative power
-averagedTrialRelPow = squeeze(mean(freqRelPow.relPowSpctrm, 1));
-trialRelPowChansAdded = add_deleted_chans(averagedTrialRelPow, EEG.chanlocs);
-trialRelPow = vertcat(trialRelPowChansAdded, averagedTrialRelPow(chanClass.EMGChans, :));
+    % Average relative power across trials
+    averagedTrialRelPow = squeeze(mean(freqRelPow.powspctrm, 1));
+    trialRelPowChansAdded = add_deleted_chans(averagedTrialRelPow, EEG.chanlocs);
 
-relPowDelta = trialRelPow(:, (round(freqRelPow.freq) >=freqBands.delta(1) & round(freqRelPow.freq) <=freqBands.delta(end)));
-relPowTheta = trialRelPow(:, (round(freqRelPow.freq) >=freqBands.theta(1) & round(freqRelPow.freq) <=freqBands.theta(end)));
-relPowAlphaMu = trialRelPow(:, (round(freqRelPow.freq) >=freqBands.alphaMu(1) & round(freqRelPow.freq) <=freqBands.alphaMu(end)));
-relPowBeta1 = trialRelPow(:, (round(freqRelPow.freq) >=freqBands.beta1(1) & round(freqRelPow.freq) <=freqBands.beta1(end)));
-relPowBeta2 = trialRelPow(:,(round(freqRelPow.freq) >=freqBands.beta2(1) & round(freqRelPow.freq) <=freqBands.beta2(end)));
-relPowGamma1 = trialRelPow(:, (round(freqRelPow.freq) >=freqBands.gamma1(1) & round(freqRelPow.freq) <=freqBands.gamma1(end)));
-relPowGamma2 = trialRelPow(:, (round(freqRelPow.freq) >=freqBands.gamma2(1) & round(freqRelPow.freq) <=freqBands.gamma2(end)));
-relPowAllFreq = trialRelPow(:, :);
+    % Frequency bands
+    bandNames = fieldnames(freqBands);
+    relPowData = struct();
 
+    % Extract power for each frequency band
+    for b = 1:length(bandNames)
+        band = bandNames{b};
+        freqIdx = round(freqRelPow.freq) >= freqBands.(band)(1) & round(freqRelPow.freq) <= freqBands.(band)(2);
+        relPowData.(band) = trialRelPowChansAdded(:, freqIdx);
+    end
 
-for roiIndex = 1:length(ROI)
-    relPow.delta.(ROI{1, roiIndex}) = mean(mean(relPowDelta(ROI{2, roiIndex})));
-    relPow.theta.(ROI{1, roiIndex}) = mean(mean(relPowTheta(ROI{2, roiIndex})));
-    relPow.alphaMu.(ROI{1, roiIndex}) = mean(mean(relPowAlphaMu(ROI{2, roiIndex})));
-    relPow.beta1.(ROI{1, roiIndex}) = mean(mean(relPowBeta1(ROI{2, roiIndex})));
-    relPow.beta2.(ROI{1, roiIndex}) = mean(mean(relPowBeta2(ROI{2, roiIndex})));
-    relPow.gamma1.(ROI{1, roiIndex}) = mean(mean(relPowGamma1(ROI{2, roiIndex})));
-    relPow.gamma2.(ROI{1, roiIndex}) = mean(mean(relPowGamma2(ROI{2, roiIndex})));
-    relPow.allFreq.(ROI{1, roiIndex}) = mean(mean(relPowAllFreq(ROI{2, roiIndex})));
-end
+    % Include all frequencies as a band
+    relPowData.allFreq = trialRelPowChansAdded;
 
-fields_A = fieldnames(relPow);
-for i = 1:length(fields_A)
-    fields_B = fieldnames(relPow.(fields_A{i}));
-end
+    % Calculate average relative power for each ROI and band
+    relPow = struct();
+    for roiIndex = 1:length(ROI)
+        regionName = ROI{1, roiIndex};
+        channelIndices = ROI{2, roiIndex};
 
+        for b = 1:length(bandNames)
+            band = bandNames{b};
+            % Compute mean relative power for the ROI in this band
+            relPow.(band).(regionName) = mean(mean(relPowData.(band)(channelIndices, :)));
+        end
 
-monsterArrayLength = length(fields_A)*length(fields_B);
-relPowValsOneLine = zeros(1, monsterArrayLength);
-for i = 1:monsterArrayLength
-    for j = 1:length(fields_A)
-        for k = 1:length(fields_B)
-            relPowValsOneLine(i) = relPow.(fields_A{j}).(fields_B{k});
-            relPowLabelOneLine{i} = strcat(fields_A{j}, '_', fields_B{k});
-            i = i+1;
+        % Compute mean relative power for all frequencies
+        relPow.allFreq.(regionName) = mean(mean(relPowData.allFreq(channelIndices, :)));
+    end
+
+    % Linearize relative power values and labels
+    fields_A = fieldnames(relPow);
+    relPowValsOneLine = [];
+    relPowLabelOneLine = {};
+
+    for i = 1:length(fields_A)
+        band = fields_A{i};
+        fields_B = fieldnames(relPow.(band));
+        for j = 1:length(fields_B)
+            regionName = fields_B{j};
+            % Append power values and labels
+            relPowValsOneLine(end+1) = relPow.(band).(regionName);
+            relPowLabelOneLine{end+1} = strcat(band, '_', regionName);
         end
     end
 end
-
-relPowValsOneLine = relPowValsOneLine(256:end);
-relPowLabelOneLine = relPowLabelOneLine(256:end);
